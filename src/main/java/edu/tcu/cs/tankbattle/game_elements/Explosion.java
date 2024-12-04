13c3c5c2dd4bac581889e5d647d471e60dd06562
Peter Ho <long.ho12021@gmail.com>
2024-12-04 11:18:03 -0600

    update explosion class

--- src/main/java/edu/tcu/cs/tankbattle/game_elements/Explosion.java
+++ src/main/java/edu/tcu/cs/tankbattle/game_elements/Explosion.java
@@ -1,43 +1,47 @@
 package edu.tcu.cs.tankbattle.game_elements;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.util.Duration;
 
 public class Explosion {
-    private final ImageView imageView;
-    private int frame = 0; // Start from frame 0
-    private Timeline animation;
+    private double x, y;
+    private boolean live = true;
+    private int step = 0;
+
+    // Load explosion images
+    private static final Image[] imgs = {
+            new Image(Explosion.class.getResource("/images/1.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/2.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/3.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/4.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/5.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/6.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/7.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/8.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/9.gif").toExternalForm()),
+            new Image(Explosion.class.getResource("/images/10.gif").toExternalForm())
+    };
 
     public Explosion(double x, double y) {
-        this.imageView = new ImageView();
-        this.imageView.setX(x);
-        this.imageView.setY(y);
+        this.x = x;
+        this.y = y;
     }
 
-    public ImageView getImageView() {
-        return imageView;
+    public boolean isLive() {
+        return live;
     }
 
-    public void play(Runnable onComplete) {
-        // Define the timeline animation
-        animation = new Timeline(new KeyFrame(Duration.millis(100), e -> {
-            if (frame <= 10) {
-                imageView.setImage(new Image(getClass().getResource("/images/" + frame + ".gif").toExternalForm()));
-                frame++;
-            } else {
-                animation.stop();
-                if (onComplete != null) {
-                    onComplete.run(); // Execute the callback
-                }
-            }
-        }));
+    public void draw(GraphicsContext gc) {
+        if (!live) {
+            return;
+        }
 
-        // Repeat for 11 frames (0 to 10)
-        animation.setCycleCount(11);
-        animation.play();
+        if (step == imgs.length) {
+            live = false;
+            return;
+        }
+
+        gc.drawImage(imgs[step], x, y);
+        step++;
     }
-
 }
